3510e7fd2b0a604e05073e694aa3d0c0d6ecc85c
Mateo <mateo.zikovic3@gmail.com>
2021-07-05 12:59:49 +0200

    commit.

--- app/src/main/java/com/application/sunbedreservation/RegistrationActivity.java
+++ app/src/main/java/com/application/sunbedreservation/RegistrationActivity.java
@@ -3,13 +3,27 @@ package com.application.sunbedreservation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    EditText first_name, last_name, e_mail, city, address, country, postal_code;
+    Button register_form_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        register_form_btn = findViewById(R.id.register_form_btn);
+        register_form_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
 
         //TODO add forms and buttons for registration, and fix form in layout
     }
